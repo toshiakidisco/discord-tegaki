cffb0378c77aa0a99593eb90e59f556a58a5d62c
toshiakidisco <toshiakidisco@gmail.com>
2024-06-22 13:03:28 +0900

    拡張機能判定 修正

--- src/tools.ts
+++ src/tools.ts
@@ -8,4 +8,12 @@ export function clamp(value: number, min: number, max: number) {
   return value;
 }
 
-export const isRunnningOnExtension = typeof chrome.runtime !== "undefined";
+export const isRunnningOnExtension = (function () {
+  if (typeof chrome === "undefined") {
+    return false;
+  }
+  if (typeof chrome.runtime !== "undefined") {
+    return false;
+  }
+  return true;
+}());
